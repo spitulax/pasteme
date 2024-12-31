72ebdde1acf144c5d9b604ecf7c88b30c4c18f84
Bintang Adiputra Pratama <bintangadiputra@proton.me>
2024-12-31 16:57:57 +0700

    nix: add meta

--- nix/default.nix
+++ nix/default.nix
@@ -28,4 +28,12 @@ stdenv.mkDerivation {
     "install"
     "PREFIX=$(out)"
   ];
+
+  meta = {
+    description = "A very simple program to quickly copy frequently used file to the current directory.";
+    mainProgram = "pasteme";
+    homepage = "https://github.com/spitulax/pasteme";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.unix;
+  };
 }
